16a8ae18f2aa1c355adc969986bbe3db84f92956
Ernest Chechelski <ernichechelski@gmail.com>
2020-04-30 08:54:25 +0200

    Initial Commit

---
+++ Requirements/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Requirements
+//
+//  Created by Ernest Chechelski on 30/04/2020.
+//  Copyright © 2020 Ernest Chechelski. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
